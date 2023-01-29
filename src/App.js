38fe51d143fced75854360e886f1ad2813971ecc
fedechaves <federicochavesf@gmail.com>
2023-01-29 16:18:26 -0300

    header component finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
